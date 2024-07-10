da180d138b998cab55900268d69697f452188331
MITTAL1104 <raghavklk11@gmail.com>
2024-07-10 16:03:49 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import PageNotFound from "./pages/PageNotFound";
+import Feed from "./pages/Feed";
+import Menu from "./pages/Menu";
+import Profile from "./pages/Profile";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Link to="/feed">Feed</Link>
+        <Link to="/menu">Menu</Link>
+        <Link to="/profile">Profile</Link>
+        <Routes>
+          <Route path="/" exact />
+          <Route path="/feed" element={<Feed />} />
+          <Route path="/menu" element={<Menu />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
